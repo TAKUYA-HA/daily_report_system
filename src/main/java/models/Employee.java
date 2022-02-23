9af0b66ba747ae6f72794342fb0e2d20bfc10545
takuya.hayase <owner_777_nerd_herd_4653@yahoo.co.jp>
2022-02-23 16:51:00 +0900

    EmployeeValidator、EmployeeService作成

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -40,7 +40,7 @@ import lombok.Setter;
 @Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
 @Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
 @NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
-@AllArgsConstructor //全てのクラスフィールドを引数にも引数ありコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
 @Entity
 public class Employee {
 
@@ -71,13 +71,13 @@ public class Employee {
     private String password;
 
     /**
-     * 管理者権限があるかどうか(一般:0、管理者:1)
+     * 管理者権限があるかどうか（一般：0、管理者：1）
      */
     @Column(name = JpaConst.EMP_COL_ADMIN_FLAG, nullable = false)
     private Integer adminFlag;
 
     /**
-     * 登録日時
+     *登録日時
      */
     @Column(name = JpaConst.EMP_COL_CREATED_AT, nullable = false)
     private LocalDateTime createdAt;
@@ -89,7 +89,7 @@ public class Employee {
     private LocalDateTime updatedAt;
 
     /**
-     * 削除された従業員かどうか(現役:0、削除済み:1)
+     * 削除された従業員かどうか（現役：0、削除済み：1）
      */
     @Column(name = JpaConst.EMP_COL_DELETE_FLAG, nullable = false)
     private Integer deleteFlag;
